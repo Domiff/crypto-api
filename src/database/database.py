d7e8da9225ffa4d5378a2cdfd4f8dbbf4137e432
dmitriy levykin <dmlegasy@gmail.com>
2026-01-18 18:38:32 +0300

    Add engine and session for app

---
+++ src/database/database.py
@@ -0,0 +1,17 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from src.settings.config import settings
+
+engine = create_async_engine(settings.database.db_url)
+async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_session():
+    async with async_session() as session:
+        yield session
+
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
