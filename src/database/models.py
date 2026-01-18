daf8851516a714e40679e9d8106acd0541d2f4a9
dmitriy levykin <dmlegasy@gmail.com>
2026-01-18 17:22:06 +0300

    Add models for app

---
+++ src/database/models.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{cls.__name__.lower()}s"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    index_price: Mapped[str]
+    instrument_name: Mapped[str]
+    timestamp: Mapped[datetime]
+
+
+class Btc(Base):
+    pass
+
+
+class Eth(Base):
+    pass
